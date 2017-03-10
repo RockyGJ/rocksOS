8d0a2aa5933ebe22864e0c965ad3921d1d2b5d82
Gertjan Rocks <gertjanrocks@outlook.com>
2017-03-10 21:10:47 +0100

    Create simple example task to test and evaluate the OS Windows version not tested!

--- examples/simple_tasks_windows.c
+++ examples/simple_tasks_windows.c
@@ -15,11 +15,14 @@
 #include "os.h"
 
 #include <windows.h>
-os_task_t test_task;
-os_task_t test_task2;
-os_task_return_codes_t test_task_callback(os_event_t event);
-os_task_return_codes_t test2_task_callback(os_event_t event);
-os_task_id_t task1, task2;
+os_task_t task1;
+os_task_t task2;
+os_task_t task3;
+os_task_return_codes_t task1_callback(os_event_t event);
+os_task_return_codes_t task2_callback(os_event_t event);
+os_task_return_codes_t task3_callback(os_event_t event);
+os_task_id_t taskID_1, taskID_2, taskID_3;
+
 os_functions_pointers_t function_pointers;
 
 void disable_interrupt(void);
@@ -38,97 +41,183 @@ void Thread( void* pParams )
 
 int main(void) {
 
-		test_task.task_cb 		= &test_task_callback;
-		test_task.task_name 	= "test";
+	//Add functions pointers necessery for interrupt
+	function_pointers.disable_irq = &disable_interrupt;
+	function_pointers.enable_irq = &enable_interrupt;
+	function_pointers.stdio = &stdio_function;
+	//Add structure to os
+	os_add_function_pointers(&function_pointers);
 
-		test_task2.task_cb 		= &test2_task_callback;
-		test_task2.task_name 	= "test2";
+	//Call init
+	os_init();
 
-		function_pointers.disable_irq = &disable_interrupt;
-		function_pointers.enable_irq = &enable_interrupt;
-		function_pointers.stdio = &stdio_function;
+	//Add task 1
+	task1.task_cb = &task1_callback;
+	task1.task_name = "task1";
+	os_add_task(task1);
+	//Add task 2
+	task2.task_cb = &task2_callback;
+	task2.task_name = "task2";
+	os_add_task(task2);
+	//Add task 3
+	task3.task_cb = &task3_callback;
+	task3.task_name = "task3";
+	os_add_task(task3);
 
-		os_add_function_pointers(&function_pointers);
-		os_init();
-		os_add_task(test_task);
-		os_add_task(test_task2);
+	//Begin thread for 1ms timing
+	_beginthread( Thread, 0, NULL );
 
-///Begin thread for 1ms timing
-		 _beginthread( Thread, 0, NULL );
+	//Start os should never return
+	os_main();
 
-		os_main();
 	return EXIT_SUCCESS;
+
 }
 
-void disable_interrupt(void){
-	os_log(os_log_level_all,"disable_interrupt\n\r");
+/**
+ * Function to disable interupt
+ */
+void disable_interrupt(void) {
+	//os_log(os_log_level_all, "disable_interrupt\n\r");
 }
 
-void enable_interrupt(void){
-	os_log(os_log_level_all,"enable_interrupt\n\r");
+/**
+ * Function to enable interrupt
+ */
+void enable_interrupt(void) {
+	//os_log(os_log_level_all, "enable_interrupt\n\r");
 }
 
-void stdio_function(void){
-	os_log(os_log_level_all,"stdio_function\n\r");
+/**
+ * Function used to output stdio
+ */
+void stdio_function(void) {
+	os_log(os_log_level_all, "stdio_function\n\r");
 }
 
-os_task_return_codes_t test_task_callback(os_event_t event)
-{
+/**
+ * Task 1
+ * @param event
+ * @return
+ */
+os_task_return_codes_t task1_callback(os_event_t event) {
 	os_msg_t msg;
-	os_timer_id_t timer;
+	static os_timer_id_t timer;
 
-	switch(event){
+	switch (event) {
 	case os_event_init:
-		os_log(os_log_level_all,"Success from init\n\r");
-		os_subscribe_for_event(os_event_msg_pending, os_current_task_id());
-		msg.os_msg_id = 10;
-		msg.data = 11;
-		timer = os_timer_add(5000,os_timer_repeat);
-		os_timer_start(timer);
+		//Save task id
+		taskID_1 = os_current_task_id();
+		//Log the task id
+		os_log(os_log_level_all, "Task %d is initialized\n\r", os_current_task_id());
+		//Subscribe for timer event
 		os_subscribe_for_event(os_event_timer, os_current_task_id());
-		task1 = os_current_task_id();
-		break;
-	case os_event_msg_pending:
-		os_retrieve_msg(&msg);
-		os_log(os_log_level_all,"Success from pending message = %d, %d\n\r",msg.os_msg_id, msg.data);
-		//msg.data++;
-		//os_post_msg(msg, task2, os_msg_priority_normal);
+		//Create timer for 10 seconds = 10000 milliseconds
+		timer = os_timer_add(10000, os_timer_repeat);
+		// Start the timer
+		os_timer_start(timer);
 		break;
 
 	case os_event_timer:
-		os_log(os_log_level_all,"Success from Timer = %d\n\r",os_current_task_id());
-		os_post_msg(msg, task2, os_msg_priority_normal);
+		os_log(os_log_level_all, "Timer send message from %d to %d\n\r",os_current_task_id(),taskID_2);
+		msg.os_msg_id = taskID_1;
+		msg.data = 0x55;
+		//Post message to task2
+		os_post_msg(msg, taskID_2, os_msg_priority_normal);
 		break;
+
 	default:
-	break;
+		break;
 	}
-	os_log(os_log_level_all,"Success from task %d %d\n\r",os_current_task_id(),event);
 	return os_task_succeed; //Succeed
 }
 
-os_task_return_codes_t test2_task_callback(os_event_t event)
-{
+
+/**
+ * Task 2
+ * @param event
+ * @return
+ */
+os_task_return_codes_t task2_callback(os_event_t event) {
 	os_msg_t msg;
-	switch(event){
+	static os_timer_id_t timer = OS_MAXIMUM_TIMERS;
+
+	switch (event) {
 	case os_event_init:
-		os_log(os_log_level_all,"Success from init\n\r");
+		//Save task id
+		taskID_2 = os_current_task_id();
+		//Log the task id
+		os_log(os_log_level_all, "Task %d is initialized\n\r", os_current_task_id());
+		//Subscribe for message pending
 		os_subscribe_for_event(os_event_msg_pending, os_current_task_id());
-		//os_subscribe_for_event(os_event_idle, os_current_task_id());
-		task2 = os_current_task_id();
+		//Subscribe for timer event
+		os_subscribe_for_event(os_event_timer, os_current_task_id());
+		//Create timer for 1 seconds = 1000 milliseconds
+		timer = os_timer_add(1000, os_timer_one_shot);
 		break;
+
 	case os_event_msg_pending:
+		//Retrieve message
 		os_retrieve_msg(&msg);
-		os_log(os_log_level_all,"Success from pending message 2 = %d, %d\n\r",msg.os_msg_id, msg.data);
-		//msg.data++;
-		//os_post_msg(msg, task1, os_msg_priority_normal);
+		os_log(os_log_level_all, "Pending message = %d, %d\n\r", msg.os_msg_id, msg.data);
+		if((msg.os_msg_id == taskID_1) && (msg.data == 0x55)){
+			// Start the timer
+			os_timer_start(timer);
+		}
 		break;
-	case os_event_idle:
-		//os_log(os_log_level_all, "idle from %d\n\r",task2);
+
+	case os_event_timer:
+		os_log(os_log_level_all, "Timer send message from %d to %d\n\r",os_current_task_id(),taskID_3);
+		msg.os_msg_id = taskID_2;
+		msg.data = 0xAA;
+		//Post message to task3
+		os_post_msg(msg, taskID_3, os_msg_priority_normal);
 		break;
 	default:
-	break;
+		break;
 	}
-	os_log(os_log_level_all,"Success from task %d %d\n\r",os_current_task_id(),event);
+	return os_task_succeed; //Succeed
+}
+
+/**
+ * Task 3
+ * @param event
+ * @return
+ */
+os_task_return_codes_t task3_callback(os_event_t event) {
+	os_msg_t msg;
+	static bool msgReceived = false;
+
+	switch (event) {
+	case os_event_init:
+		//Save task id
+		taskID_3 = os_current_task_id();
+		//Log the task id
+		os_log(os_log_level_all, "Task %d is initialized\n\r", os_current_task_id());
+		//Subscribe for message pending
+		os_subscribe_for_event(os_event_msg_pending, os_current_task_id());
+		//Subscribe for idle
+		os_subscribe_for_event(os_event_idle, os_current_task_id());
+		break;
+
+	case os_event_msg_pending:
+		//Retrieve message
+		os_retrieve_msg(&msg);
+		os_log(os_log_level_all, "Pending message = %d, %d\n\r", msg.os_msg_id, msg.data);
+		if((msg.os_msg_id == taskID_2) && (msg.data == 0xAA)){
+			msgReceived = true;
+		}
+		break;
+	case os_event_idle:
+		if(msgReceived){
+			os_log(os_log_level_all, "Task %d in idle\n\r", os_current_task_id());
+			msgReceived = false;
+		}
+		break;
+
+	default:
+		break;
+	}
 	return os_task_succeed; //Succeed
 }
 
